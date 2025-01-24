affabb762a7c6889f8240b230ef9f53f5a57bfed
Alexandre Roman <alexandre.roman@broadcom.com>
2025-01-24 16:10:21 +0100

    Init project

---
+++ src/test/java/com/broadcom/tanzu/demos/bonjour/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.broadcom.tanzu.demos.bonjour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
